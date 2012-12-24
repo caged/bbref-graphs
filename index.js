ed4bfd0b287ac37da95e72cfe3bdc4efbb434300
Justin Palmer <justin@github.com>
2012-12-24 15:14:44 -0800

    genesis

---
+++ index.js
@@ -0,0 +1,57 @@
+(function main() {
+
+  // Helper for use in event bindings
+  var bind = function(func, context) {
+    return Function.prototype.bind.apply(func, [].slice.call(arguments, 1))
+  }
+
+  function showChart(container, table) {
+    table.style('display', 'none')
+
+    if(container.select('div.graph').node() == null) {
+      container.insert('div')
+        .attr('class', 'graph')
+        .text('chart')
+    } else {
+      container.select('div.graph')
+        .style('display', 'block')
+    }
+  }
+
+  function hideChart(container, table) {
+    table.style('display', 'table')
+    container.select('div.graph').style('display', 'none')
+  }
+
+  // Chart link was clicked
+  function onChartLinkClick(link, event) {
+    var container = d3.select(this.parentNode),
+        table     = container.select('.stats_table')
+
+    if(table.style('display') == 'none') {
+      hideChart.apply(this, [container, table, event])
+      link.innerText = 'Chart'
+    } else {
+      showChart.apply(this, [container, table, event])
+      link.innerText = 'Table'
+    }
+  }
+
+
+
+  var headings = document.querySelectorAll('#basic_div .table_heading, #advanced_div .table_heading'),
+      i = 0,
+      len = headings.length
+
+  for(i; i < len; i++) {
+    var heading = headings[i]
+        a = document.createElement('a')
+
+    a.innerText = 'Chart'
+    a.href = '#chart'
+    a.addEventListener('click', bind(onChartLinkClick, heading, a))
+
+    heading.appendChild(a)
+  }
+
+})()
